8c53562c8f1db06dec5515763e4c39ab636e8717
RICARDO MOYA GARCIA <ricardo.moyagarcia@telefonica.com>
2022-10-07 00:04:18 +0200

    delete constants file for NoModuleError and upload min python version

--- xaiographs/viz/launcher.py
+++ xaiographs/viz/launcher.py
@@ -8,10 +8,38 @@ import webbrowser
 
 from absl import app, flags
 
-from xaiographs.common.constants import BACKEND_NODE_PACKAGE, BACKEND_PUBLIC_FOLDER, CSV_FILES, FRONTEND_NODE_PACKAGE, \
-    COL_HEIGHT_LEFT, MY_PROTOCOL, MY_HOST_NAME, MY_HTML_FOLDER_PATH, MY_HOME_PAGE_FILE_PATH, NODE_BACKEND, \
-    NODE_FRONTEND, NODE_MODULES_BACKEND, NODE_MODULES_FRONTEND, COL_HEIGHT_RIGHT, TEXT_COLOR_WHITE, TEXT_COLOR_RED, \
-    TEXT_COLOR_GREEN, XAIOWEB_DISTRIBUTION
+# CONSTANTS
+BACKEND_NODE_PACKAGE = 'Backend node package'
+BACKEND_PUBLIC_FOLDER = 'backend/public'
+COL_HEIGHT_LEFT = 50
+COL_HEIGHT_RIGHT = 20
+FRONTEND_NODE_PACKAGE = 'Frontend node package'
+MY_PROTOCOL = 'http://'
+MY_HOST_NAME = 'localhost'
+MY_HTML_FOLDER_PATH = 'XAIoWeb/'
+MY_HOME_PAGE_FILE_PATH = 'index.html'
+NODE_BACKEND = 'backend'
+NODE_FRONTEND = 'frontend'
+NODE_MODULES_BACKEND = 'backend/node_modules'
+NODE_MODULES_FRONTEND = 'frontend/node_modules'
+TEXT_COLOR_WHITE = '\033[0m'
+TEXT_COLOR_RED = '\033[91m'
+TEXT_COLOR_GREEN = '\033[92m'
+XAIOWEB_DISTRIBUTION = 'XAIoWeb distribution'
+
+CSV_FILES = ["data_correlation_matrix_dataset.csv",
+             "input_dataset.csv",
+             "local_explainability_graph_nodes_weights.csv",
+             "global_explainability.csv",
+             "input_dataset_discretized.csv",
+             "local_reason_why.csv",
+             "global_explainability_graph_edges_weights.csv",
+             "local_dataset_reliability.csv",
+             "target_distribution.csv",
+             "global_explainability_graph_nodes_weights.csv",
+             "local_explainability.csv",
+             "global_target_explainability.csv",
+             "local_explainability_graph_edges_weights.csv"]
 
 # Parameters definition
 flags.DEFINE_integer(name='port', default=8080, help='Web server port', required=False)
