fda6def5e792b0065c13b6a7279a67e5046abe63
RICARDO MOYA GARCIA <ricardo.moyagarcia@telefonica.com>
2022-10-06 22:52:02 +0200

    windows launcher + FLASGS of absl library + run app absl + constants file

---
+++ xaiographs/common/constants.py
@@ -0,0 +1,32 @@
+# LAUNCHER
+BACKEND_NODE_PACKAGE = 'Backend node package'
+BACKEND_PUBLIC_FOLDER = 'backend/public'
+COL_HEIGHT_LEFT = 50
+COL_HEIGHT_RIGHT = 20
+FRONTEND_NODE_PACKAGE = 'Frontend node package'
+MY_PROTOCOL = 'http://'
+MY_HOST_NAME = 'localhost'
+MY_HTML_FOLDER_PATH = 'XAIoWeb/'
+MY_HOME_PAGE_FILE_PATH = 'index.html'
+NODE_BACKEND = 'backend'
+NODE_FRONTEND = 'frontend'
+NODE_MODULES_BACKEND = 'backend/node_modules'
+NODE_MODULES_FRONTEND = 'frontend/node_modules'
+TEXT_COLOR_WHITE = '\033[0m'
+TEXT_COLOR_RED = '\033[91m'
+TEXT_COLOR_GREEN = '\033[92m'
+XAIOWEB_DISTRIBUTION = 'XAIoWeb distribution'
+
+CSV_FILES = ["data_correlation_matrix_dataset.csv",
+             "input_dataset.csv",
+             "local_explainability_graph_nodes_weights.csv",
+             "global_explainability.csv",
+             "input_dataset_discretized.csv",
+             "local_reason_why.csv",
+             "global_explainability_graph_edges_weights.csv",
+             "local_dataset_reliability.csv",
+             "target_distribution.csv",
+             "global_explainability_graph_nodes_weights.csv",
+             "local_explainability.csv",
+             "global_target_explainability.csv",
+             "local_explainability_graph_edges_weights.csv"]
