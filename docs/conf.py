7cea84235fbd9e0a8d06360a5dab060cb22d6156
RicardoMoya <52276729+TFRicardoMoya@users.noreply.github.com>
2022-11-08 17:23:40 +0100

    Update conf.py
    Delete copyright

--- docs/conf.py
+++ docs/conf.py
@@ -1,12 +1,3 @@
-# -*- coding: utf-8 -*-
-
-u"""
-(c) Copyright 2021 Telefónica. All Rights Reserved.
-The copyright to the software program(s) is property of Telefónica.
-The program(s) may be used and or copied only with the express written consent
-of Telefónica or in accordance with the terms and conditions stipulated in the
-agreement/contract under which the program(s) have been supplied."""
-
 # Configuration file for the Sphinx documentation builder.
 #
 # This file only contains a selection of the most common options. For a full
