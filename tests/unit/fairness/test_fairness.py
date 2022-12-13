4a881f7068486c0571c9adbecf723334c27b5356
RICARDO MOYA GARCIA <ricardo.moyagarcia@telefonica.com>
2022-12-13 13:38:26 +0100

    draft V fairness module

--- tests/unit/fairness/test_fairness.py
+++ tests/unit/fairness/test_fairness.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as pd
 import unittest
 
-from xaiographs.fairness.fairness import Fairness
+from xaiographs.fairness import Fairness
 
 pd.set_option('display.max_columns', None)
 pd.set_option('display.max_rows', None)
