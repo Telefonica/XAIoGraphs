87d0c285499ed21b55ef94b4a942ef1c84b3b393
TFRicardoMoya <ricardo.moyagarcia@telefonica.com>
2025-11-04 18:34:26 +0100

    minor doc titanic example

--- examples/titanic_example.py
+++ examples/titanic_example.py
@@ -8,7 +8,7 @@ XAIoGraphs is free software: you can redistribute it and/or modify it under the 
 License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any
 later version.
 
-XAIoGraphs is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+XAIoGraphs are distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
 warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the Affero GNU General Public License
 for more details.
 
