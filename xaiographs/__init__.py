934ddbeeac0da741be11ff0927697a31557ca00a
TFRicardoMoya <ricardo.moyagarcia@telefonica.com>
2025-11-04 13:48:32 +0100

    updated to python version >= 3.10 & < 3.13

--- xaiographs/__init__.py
+++ xaiographs/__init__.py
@@ -23,19 +23,19 @@ from .fairness import Fairness
 from .why.why import Why
 
 # Check the Python version
-if sys.version_info < (3, 7) or sys.version_info >= (3, 10):
+if sys.version_info < (3, 10) or sys.version_info >= (3, 13):
     raise RuntimeError("Your Python {}.{}.{} version is incompatible with XAIoGraphs.\n"
-                       "XAIoGraphs requires Python 3.7 or higher, but less than 3.10."
+                       "XAIoGraphs requires Python 3.10 or higher, but less than 3.13."
                        .format(sys.version_info.major, sys.version_info.minor, sys.version_info.micro))
 
 # Check the numpy version
 numpy_version = np.__version__
 
-# Split the version into a tuple of integers (e.g., '1.20.3' -> (1, 20, 3))
+# Split the version into a tuple of integers (e.g., '1.26.4' -> (1, 26, 4))
 numpy_version_tuple = tuple(map(int, numpy_version.split('.')))
 
-# Check if the version is within the allowed range (>=1.19.5, <1.23.5)
-if not ((1, 19, 5) <= numpy_version_tuple <= (1, 23, 1)):
+# Check if the version is within the allowed range (>=1.26.0, <2.0.0)
+if not ((1, 26, 0) <= numpy_version_tuple < (2, 0, 0)):
     raise ImportError("Your numpy version {} is incompatible with XAIoGraphs.\n"
-                      "XAIoGraphs requires numpy version 1.19.5 or higher, but less than 1.23.1"
+                      "XAIoGraphs requires numpy version 1.26.0 or higher, but less than 2.0.0"
                       .format(numpy_version))
